72a30ce275f38c4a842d6a20b9a9d80b5d9fcc7c
Michael Hebblethwaite <mhhebble@gmail.com>
2018-03-20 15:08:52 +1100

    class mockup

---
+++ Michael/wk02/2-tue/js_data_type/data_type.js
@@ -0,0 +1,52 @@
+typeof 15
+// Prediction: number
+// Actual: number
+
+typeof 5.5
+// Prediction: number
+// Actual: number
+
+typeof NaN
+// Prediction: number
+// Actual: number
+
+typeof "hello"
+// Prediction: string
+// Actual: string
+
+typeof true
+// Prediction: boolean
+// Actual: boolean
+
+typeof 1 != 2
+// Prediction: boolean
+// Actual: true
+
+
+"hamburger" + "s"
+// Prediction: "hamburgers"
+// Actual:
+
+"hamburgers" - "s"
+// Prediction: error
+// Actual: NaN
+
+"1" + "3"
+// Prediction: "13"
+// Actual: "13"
+
+"1" - "3"
+// Prediction: NaN
+// Actual: -2
+
+"johnny" + 5
+// Prediction: johnny5
+// Actual: johnny5
+
+"johnny" - 5
+// Prediction: NaN
+// Actual: NaN
+
+99 * "luftbaloons"
+// Prediction: NaN
+// Actual NaN
