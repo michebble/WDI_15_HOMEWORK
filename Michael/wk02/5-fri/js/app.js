2bb1df64af5aae967a0e6dfb7895e17289561ec0
Michael Hebblethwaite <mhhebble@gmail.com>
2018-03-25 16:34:32 +1100

    basic cat walk back and forth, plus stop and speed up

--- Michael/wk02/5-fri/js/app.js
+++ Michael/wk02/5-fri/js/app.js
@@ -3,6 +3,7 @@ console.log('Meow-tastic')
 var movePixels = 10;
 var delayMs = 50;
 var catTimer = null;
+var walkLeft = true;
 
 var startButton = document.querySelector('#start-button');
 var speedButton = document.querySelector('#speed-button');
@@ -12,26 +13,50 @@ var screenSize = document.querySelector('#info');
 var catWalk = function() {
   var img = document.querySelector('img');
   var currentLeft = parseInt(img.style.left);
+  img.style.transform = 'scaleX(1)'
   img.style.left = (currentLeft + movePixels) + 'px';
   if (currentLeft > (window.innerWidth - img.width)) {
-    img.style.left = '0px';
+    clearInterval(catTimer);
+    walkLeft = false;
+    startCatWalk();
   }
 }
 
+var catWalkBack = function() {
+  
+  var img = document.querySelector('img');
+  var currentLeft = parseInt(img.style.left);
+  img.style.transform = 'scaleX(-1)'
+  img.style.left = (currentLeft - movePixels) + 'px';
+  if (currentLeft < (window.innerWidth - window.innerWidth)) {
+    clearInterval(catTimer);
+    walkLeft = true;
+    startCatWalk();
+  }
+}
+    
 var startCatWalk = function() {
-  catTimer = window.setInterval(catWalk, delayMs);
+  startButton.disabled = true;
+  if (walkLeft === true) {
+    catTimer = window.setInterval(catWalk, delayMs);
+  } else if (walkLeft === false) {
+    catTimer = window.setInterval(catWalkBack, delayMs);
+  }
 }
 
 var stopCatWalk = function() {
-  movePixels = 0;
-  catTimer = null;
+  startButton.disabled = false;
+  movePixels = 10;
+  clearInterval(catTimer);
 }
 
-var windowResize = function() {
-
+var speedUp = function() {
+  movePixels = movePixels + 5;
 }
 
 
-info.addEventListener('resize', )
 startButton.addEventListener('click', startCatWalk);
 stopButton.addEventListener('click', stopCatWalk);
+speedButton.addEventListener('click', speedUp);
+
+// currentLeft + 138 > (window.innerWidth / 2 ) centre point
