bad1f4b4a42f169cf40fb6e71b5621b7b053c529
Michael Hebblethwaite <mhhebble@gmail.com>
2018-04-04 21:17:18 +1000

    renamed some functions, arguments, and variables for clarity

--- Michael/wk01/5-fri/ptv_exercise/ptv.js
+++ Michael/wk01/5-fri/ptv_exercise/ptv.js
@@ -39,15 +39,19 @@ var tempRouteArr1 = []
 var tempRouteArr2 = []
 var tempIndex = []
 var lookUpArr = []
+var originLineIndex
+var originStationIndex
+var destinationLineIndex
+var destinationStationIndex
 
 //Initialise connection. This should be replaced with an array comparison that scans two arrays and finds matching elements
 var connection = 'Richmond'
 
 //Initialise functions
-var stopsLookUp = function(station) {  //take Origin and Destination as station name to look up.
+var stopsLookUp = function(stationName) {  //take Origin and Destination as station name to look up.
   for (stopsArr = 0; stopsArr < lines.length; stopsArr++) { // move through each object in line array
     for (stopIndex = 0; stopIndex < lines[stopsArr].stops.length; stopIndex++) { // through stops array in the current object
-      if (station === lines[stopsArr].stops[stopIndex]) { //test if station name is present in array
+      if (stationName === lines[stopsArr].stops[stopIndex]) { //test if station name is present in array
         console.log(stopsArr)  // testing returns object number
         console.log(stopIndex) // testing returns array from object
         return [lines[stopsArr].stops, stopIndex]
@@ -65,19 +69,7 @@ var connectionLookUp = function(station, arr) {
   }
 }
 
-
-
-var reverser = function(boolean, arr, elemIndex1, elemIndex2) {     // I dont like this one. non boolean parameters should be numbers not arrays.
-  if (boolean === true) { 
-    var finalRoute = arr.slice(elemIndex1, elemIndex2 + 1)
-  } else if (boolean === false) {
-    var finalRoute = arr.slice(elemIndex2, elemIndex1 + 1)
-    finalRoute.reverse()
-  }
-  return finalRoute
-}
-
-var compareElement = function(firstStop, lastStop) {
+var compareStations = function(firstStop, lastStop) {
   if (firstStop > lastStop) {
     return false
   } else if (firstStop < lastStop) {
@@ -85,6 +77,23 @@ var compareElement = function(firstStop, lastStop) {
   }
 }
 
+var reverser = function(arr, elemIndex1, elemIndex2) {     // I dont like this one. non boolean parameters should be numbers not 
+  var isOriginFirst = compareStations(elemIndex1, elemIndex2)
+  if (isOriginFirst === true) { 
+    var finalRoute = arr.slice(elemIndex1, elemIndex2 + 1)
+  } else if (isOriginFirst === false) {
+    var finalRoute = arr.slice(elemIndex2, elemIndex1 + 1)
+    finalRoute.reverse()
+  }
+  return finalRoute
+}
+
+
+
+
+var findStation = function (message, ){
+
+}
 
 // prompt for stations
 finalOutput.origin = prompt('Enter station to depart.')
@@ -111,13 +120,13 @@ if (lookUpArr[0][0] === lookUpArr[1][0]) { // if yes, move routeDestin[1] to new
   finalOutput.originIndex = lookUpArr[0][1] //kept this verbose for readability
   finalOutput.destinIndex = lookUpArr[1][1]
   tempRouteArr1 = lookUpArr[0][0]
-  finalOutput.stopsFinal = reverser(compareElement(finalOutput.originIndex, finalOutput.destinIndex), tempRouteArr1, finalOutput.originIndex, finalOutput.destinIndex)
+  finalOutput.stopsFinal = reverser(tempRouteArr1, finalOutput.originIndex, finalOutput.destinIndex)
 } else if (lookUpArr[0][0] !== lookUpArr[1][0]) { // if not,
     // console.log('different routes!')   //testing
     tempIndex[0] = connectionLookUp(connection, lookUpArr[0][0]) //find location of Richmond in first array
-    tempRouteArr1 = reverser(compareElement(lookUpArr[0][1], tempIndex[0]), lookUpArr[0][0], lookUpArr[0][1], tempIndex[0]) // order array so Richmond is last and then cut
+    tempRouteArr1 = reverser( lookUpArr[0][0], lookUpArr[0][1], tempIndex[0]) // order array so Richmond is last and then cut
     tempIndex[1] = connectionLookUp(connection, lookUpArr[1][0]) //find location of Richmond in second array
-    tempRouteArr2 = reverser(compareElement(tempIndex[1], lookUpArr[1][1]), lookUpArr[1][0], tempIndex[1], lookUpArr[1][1]) // order array so Richmond is first and then cut
+    tempRouteArr2 = reverser( lookUpArr[1][0], tempIndex[1], lookUpArr[1][1]) // order array so Richmond is first and then cut
     tempRouteArr2.shift() // remove richmond from second array
     finalOutput.stopsFinal = tempRouteArr1.concat(tempRouteArr2) // concat arrays together to produce complete line
 }
