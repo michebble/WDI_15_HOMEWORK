28dbbed0c10b3fb2367cf80f7a955a9123c759f1
Michael Hebblethwaite <mhhebble@gmail.com>
2018-04-05 22:21:23 +1000

    create basic functionality of stock app

---
+++ Michael/wk04/4-thu/stock_app/main.rb
@@ -0,0 +1,13 @@
+require 'sinatra'
+require 'sinatra/reloader'
+require 'stock_quote'
+require 'pry'
+
+get '/' do
+  if params[:stock_symbol] != nil
+  @stock = StockQuote::Stock.quote(params[:stock_symbol])
+  else
+    @stock = nil
+  end
+  erb :index
+end
