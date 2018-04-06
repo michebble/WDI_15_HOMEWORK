a80d90079455158f269c733752f92f75e5e622d6
Michael Hebblethwaite <mhhebble@gmail.com>
2018-04-06 13:02:50 +1000

    learn layout.erb

--- Michael/wk04/4-thu/stock_app/main.rb
+++ Michael/wk04/4-thu/stock_app/main.rb
@@ -4,14 +4,35 @@ require 'stock_quote'
 require 'pry'
 
 get '/' do
-  if params[:stock_symbol] != nil
-  @stock = StockQuote::Stock.quote(params[:stock_symbol])
-  else
-    @stock = nil
+  @title = "Check your Stocks"
+  if params[:stock_symbol] == ""
+    redirect to('/')
+    @stock_symbol= ""
+    @company_name= ""
+    @latest_price = ""
+  elsif params[:stock_symbol] != nil
+    stock = StockQuote::Stock.quote(params[:stock_symbol])
+    @stock_symbol = "('#{stock.symbol}')"
+    @company_name = stock.company_name
+    @latest_price = stock.latest_price
   end
   erb :index
+  
+  # if params[:stock_symbol] != ""
+  # @stock = StockQuote::Stock.quote(params[:stock_symbol])
+  # else
+  #   #@stock = nil
+  #   redirect to('/')
+  # end
+  # erb :index
 end
 
+
+
+
+
+
 get '/about' do
+  @title = "ABOUT"
   erb :about
 end
