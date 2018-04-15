51dbac51f652e68a4b061f9221689c29afce798a
Michael Hebblethwaite <mhhebble@gmail.com>
2018-04-15 12:02:56 +1000

    finish nba sql commands

---
+++ Michael/wk05/5-fri/say_in_english/say_in_english.rb
@@ -0,0 +1,23 @@
+class Say
+
+  def initialize(number)
+    if number.length == 2 && number
+      
+      
+    else 
+    end
+  end
+
+  zero = "zero"
+  ones = ["one", "two", "three", "four", "five", "six", "seven," "eight", "nine"]
+  teens = ["eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"]
+  tens = ["ten", "twenty", "thirty", "fourty", "fifty", "sixty", "seventy", "eighty", "ninty"]
+
+
+
+end
+
+if number == 0
+  puts zero
+else
+  puts ones[number-1]
