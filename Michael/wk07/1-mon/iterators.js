95508c377b0397f9934710e3c6a685b76079257b
Michael Hebblethwaite <mhhebble@gmail.com>
2018-04-24 15:45:24 +1000

    refactor js iterators

--- Michael/wk07/1-mon/iterators.js
+++ Michael/wk07/1-mon/iterators.js
@@ -1,17 +1,25 @@
-var countInteger = function(array) {
-  var output = 0;
-  array.forEach(function(element){
-    if (Number.isInteger(element)) {
-      output += 1;
-    } 
-  });
-  return output;
-}
+// var countInteger = function(array) {
+//   var listOfIntegers = [];
+//   array.forEach(function(element){
+//     if (Number.isInteger(element)) {
+//       listOfIntegers.push(element);
+//     } 
+//   });
+//   return listOfIntegers;
+// }
 
-countInteger([4, 2.2, 5, 6, 4.2, 8.2, 4]);
+// countInteger([4, 2.2, 5, 6, 4.2, 8.2, 4]);
 
 var getSquares = function(array) {
-  return array.map(number => number**2);
+
+  var squareEachNum = function(number){ 
+    return Math.pow(number, 2);
+  }
+
+  var squaredNums = array.map(squareEachNum);
+  
+  return squaredNums;
 }
 
 getSquares([1, 2, 3, 4, 5]);
+
