b854fb5e4f601e34699cafc55ac62f015c8b7d97
Michael Hebblethwaite <mhhebble@gmail.com>
2018-04-26 11:54:54 +1000

    add semi-colons

--- Michael/wk07/2-tue/jsmovies/app.js
+++ Michael/wk07/2-tue/jsmovies/app.js
@@ -1,10 +1,16 @@
 
 var createList = function(res) {
   res.Search.forEach(function(movie){
-    $h2 = $('<h2>')
-    $a = $('<a>').attr('href',`https://www.imdb.com/title/${movie.imdbID}`).attr('target', '_blank').text(movie.Title)
-    $h2.append($a)
-    $('main').append($h2)
+    $h2 = $('<h2>');
+
+    $a = $('<a>')
+      .attr('href',`https://www.imdb.com/title/${movie.imdbID}`)
+      .attr('target', '_blank')
+      .text(movie.Title);
+
+    $h2.append($a);
+
+    $('main').append($h2);
   })
 }
 
@@ -12,12 +18,11 @@ var $searchBtn = $('.search-btn');
 
 $searchBtn.on('click', function(event){
   event.preventDefault();
-  var $searchBar = $(".search-bar");
-  var $searchQuery = $searchBar.val();
+  var $searchBar = $(".search-bar"); 
   var options = {
-    url: 'http://omdbapi.com/?s=' + $searchQuery + '&apikey=2f6435d9'
-  }
-  $.ajax(options).done(createList)
+    url: 'http://omdbapi.com/?s=' + $searchBar.val() + '&apikey=2f6435d9'
+  };
+  $.ajax(options).done(createList);
 });
     
 
