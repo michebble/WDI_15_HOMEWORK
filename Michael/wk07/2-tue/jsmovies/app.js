8dfff2855e6ceb229bca65ffaebde2370267ba5c
Michael Hebblethwaite <mhhebble@gmail.com>
2018-04-26 10:59:56 +1000

    add new tab function

--- Michael/wk07/2-tue/jsmovies/app.js
+++ Michael/wk07/2-tue/jsmovies/app.js
@@ -1,7 +1,7 @@
 
 var createList = function(object) {
   object.Search.forEach(function(element){
-    $('main').append("<a href=https://www.imdb.com/title/" + element.imdbID + "><h2>" + element.Title + "</h2></a>")
+    $('main').append("<a href=https://www.imdb.com/title/" + element.imdbID + " target='_blank'><h2>" + element.Title + "</h2></a>")
   })
 }
 
