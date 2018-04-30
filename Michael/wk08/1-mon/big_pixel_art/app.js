a0ee1398089b3fa33cb432f5f44e0af4238124e3
Michael Hebblethwaite <mhhebble@gmail.com>
2018-04-30 22:45:32 +1000

    add background image selection

--- Michael/wk08/1-mon/big_pixel_art/app.js
+++ Michael/wk08/1-mon/big_pixel_art/app.js
@@ -1,9 +1,11 @@
 $brushBox = $('.brush-box');
 $colorInput = $('.color-input');
 $colorBtn = $('.color-btn');
+$searchInput = $('.search-input');
+$searchBtn = $('.search-btn');
 
 var changeColor = function(event) {
-  var currentColor = $brushBox[0].style.backgroundColor
+  var currentColor = $brushBox[0].style.backgroundColor;
   event.target.style.backgroundColor = currentColor;
 }
 
@@ -17,16 +19,33 @@ var createPixels = function() {
 }
 
 var setColor = function() {
-  var color = $colorInput[0].value;
-  $brushBox[0].style.backgroundColor = color;
+  var color = $colorInput.val();
+  $brushBox.css('background-color', color);
 }
 
+var setBackgroundImage = function(res) {
+  var moviePoster = res.Poster;
+  $('main').css('background-image', 'url(' + moviePoster + ')');
+}
 
 $colorInput.keypress(function(event) {
   if (event.which == 13) {
     setColor();
   }
 })
+
+$searchBtn.click(function(event){
+  
+  event.preventDefault();
+  var movieTitle = $searchInput.val();
+  
+  var options = {
+    url: 'http://omdbapi.com/?t=' + movieTitle + '&apikey=2f6435d9'
+  };
+  $.ajax(options).done(setBackgroundImage);
+});
+
+
 
 $colorBtn.click(function(event) {
   event.preventDefault();
