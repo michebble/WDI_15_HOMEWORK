e249d777fb105ec53e02b97ed7cc8ec47b73f9fa
Michael Hebblethwaite <mhhebble@gmail.com>
2018-04-30 16:06:59 +1000

    add set color on pressing 'enter' when input is in focus

--- Michael/wk08/1-mon/big_pixel_art/app.js
+++ Michael/wk08/1-mon/big_pixel_art/app.js
@@ -2,14 +2,23 @@ $brushBox = $('.brush-box');
 $colorInput = $('.color-input');
 $colorBtn = $('.color-btn');
 
+var setColor = function() {
+  
+  var color = $colorInput[0].value;
+  
+  $brushBox[0].style.backgroundColor = color;
+}
+
+$colorInput.on('keypress', function(event) {
+  if (event.which == 13) {
+    setColor();
+  }
+})
+
 $colorBtn.on('click', function(event) {
   event.preventDefault();
 
-  $colorInput = $('.color-input');
-  var color = $colorInput[0].value;
-  
-  $brushBox = $('.brush-box');
-  $brushBox[0].style.backgroundColor = color;
+  setColor();
 
 })
 
