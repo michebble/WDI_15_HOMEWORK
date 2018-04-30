27666826c1c83f6ff813bcab586c3aedcfb47f6c
Michael Hebblethwaite <mhhebble@gmail.com>
2018-04-30 16:17:52 +1000

    append grid of 100 20px squares

--- Michael/wk08/1-mon/big_pixel_art/app.js
+++ Michael/wk08/1-mon/big_pixel_art/app.js
@@ -2,12 +2,28 @@ $brushBox = $('.brush-box');
 $colorInput = $('.color-input');
 $colorBtn = $('.color-btn');
 
+
+
+var createPixels = function() {
+  for(var i = 0; i < 100; i++) {
+    $div = $('<div>')
+      .addClass('square');
+
+    $('main').append($div);
+  }
+}
+
+
 var setColor = function() {
   
   var color = $colorInput[0].value;
   
   $brushBox[0].style.backgroundColor = color;
 }
+
+
+createPixels();
+
 
 $colorInput.on('keypress', function(event) {
   if (event.which == 13) {
