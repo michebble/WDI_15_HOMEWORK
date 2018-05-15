1f318b1bf7686db7d0700720f69cfb2b94d0ec4c
Michael Hebblethwaite <mhhebble@gmail.com>
2018-05-15 15:57:27 +1000

    add button event handlers

--- Michael/wk10/2-tue/callback/timers.js
+++ Michael/wk10/2-tue/callback/timers.js
@@ -1,3 +1,7 @@
 var resetBtn = document.querySelector('#reset');
 var startBtn = document.querySelector('#start');
 var pauseBtn = document.querySelector('#pause');
+
+resetBtn.addEventListener('click',  );
+startBtn.addEventListener('click',  );
+pauseBtn.addEventListener('click',  );
