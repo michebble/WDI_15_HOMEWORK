c3cb7a0d39b90a0501ffb158888867d3a66aa79a
Michael Hebblethwaite <mhhebble@gmail.com>
2018-05-15 17:03:46 +1000

    add reset function

--- Michael/wk10/2-tue/callback/timers.js
+++ Michael/wk10/2-tue/callback/timers.js
@@ -3,19 +3,25 @@ var startBtn = document.querySelector('#start');
 var pauseBtn = document.querySelector('#pause');
 var timer = document.querySelector('#timer');
 
-
-
 var seconds = 0;
 var timerId;
+var readyMessage = 'Stop Watch';
 
 function updateTimer() {
   seconds += 1;
   timer.textContent = seconds;
 }
 
-resetBtn.addEventListener('click', function(){});
+resetBtn.addEventListener('click', function(){
+  clearInterval(timerId);
+  seconds = 0;
+  timer.textContent = readyMessage;
+});
+
 startBtn.addEventListener('click', function(){
   timerId = setInterval(function(){updateTimer()}, 1000);
 });
-pauseBtn.addEventListener('click', function(){clearInterval(timerId);
+
+pauseBtn.addEventListener('click', function(){
+  clearInterval(timerId);
 });
