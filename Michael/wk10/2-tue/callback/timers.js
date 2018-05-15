4a8e95e8459723339ec299d45ea896c22de18a93
Michael Hebblethwaite <mhhebble@gmail.com>
2018-05-15 16:51:29 +1000

    update start button click handler

--- Michael/wk10/2-tue/callback/timers.js
+++ Michael/wk10/2-tue/callback/timers.js
@@ -1,16 +1,20 @@
 var resetBtn = document.querySelector('#reset');
 var startBtn = document.querySelector('#start');
 var pauseBtn = document.querySelector('#pause');
+var timer = document.querySelector('#timer');
 
-resetBtn.addEventListener('click',  );
-startBtn.addEventListener('click',  );
-pauseBtn.addEventListener('click',  );
+
 
 var seconds = 0;
-var timerId = setInterval(function(){}, 1000);
+var timerId;
 
 function updateTimer() {
-  var timer = document.querySelector('#timer');
   seconds += 1;
   timer.textContent = seconds;
 }
+
+resetBtn.addEventListener('click', function(){} );
+startBtn.addEventListener('click', function(){
+  timerId = setInterval(function(){updateTimer()}, 1000)
+} );
+pauseBtn.addEventListener('click', function(){}  );
