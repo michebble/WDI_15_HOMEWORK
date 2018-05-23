c41a5306e6783ae0593e716126622f360438628b
Michael Hebblethwaite <mhhebble@gmail.com>
2018-05-23 15:12:20 +1000

    write lowercase test

--- Michael/wk11/3-wed/webpack3-react-starter/test/passwordMeter.test.js
+++ Michael/wk11/3-wed/webpack3-react-starter/test/passwordMeter.test.js
@@ -7,7 +7,9 @@ describe('passwordMeter', () => {
     expect(getStrength('')).to.be.equal(0)
   })
   it('should return 1 when password has eight chars', () => {
-    expect(getStrength('abcdefgh')).to.deep.equal(1)
-      
+    expect(getStrength('abcdefgh')).to.deep.equal(1)  
+  })
+  it('should return 1 when password has a lowercase char and is less than eight', () => {
+    expect(getStrength('abc')).to.deep.equal(1)
   })
 })
