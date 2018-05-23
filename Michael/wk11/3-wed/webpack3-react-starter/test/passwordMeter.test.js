e5cc62d79c63a24ee35214e8c147ede005cf5fa7
Michael Hebblethwaite <mhhebble@gmail.com>
2018-05-23 15:21:15 +1000

    write number test

--- Michael/wk11/3-wed/webpack3-react-starter/test/passwordMeter.test.js
+++ Michael/wk11/3-wed/webpack3-react-starter/test/passwordMeter.test.js
@@ -15,4 +15,10 @@ describe('passwordMeter', () => {
   it('should return 1 when password has a uppercase char and is less than eight', () => {
     expect(getStrength('ABC')).to.be.equal(1)
   })
+  it('should return 1 when password has a uppercase char and is less than eight', () => {
+    expect(getStrength('ABC')).to.be.equal(1)
+  })
+  it('should return 1 when password has a number char and is less than eight', () => {
+    expect(getStrength('123')).to.be.equal(1)
+  })
 })
